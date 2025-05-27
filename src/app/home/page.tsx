e9c7230905081ec2b87c60504d7b8ed1a325ba46
Matheus Leite <matheusssleite@outlook.com>
2025-05-27 18:58:48 -0300

    Colocando os hovers nas imagens e nos botões

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -31,20 +31,20 @@ export default function Home() {
                 <div className="max-w-[60%]">
                     <h4 className="text-2xl text-gray-600">Encontre uma marca que você conhece e confia.</h4>
                     <div id="carrouselMarcas" className="max-w-[100%] overflow-auto mt-8">
-                        <div className="flex gap-6 w-full">
-                            <Image width={200} height={200} className="cursor-pointer" src="/madeiramadeira.png" alt="MadeiraMadeira" onClick={() => window.open("https://www.madeiramadeira.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/tokstok.png" alt="Tok&Stock" onClick={() => window.open("https://www.tokstok.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/casasbahia.png" alt="Casas Bahia" onClick={() => window.open("https://www.casasbahia.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/mobly.png" alt="Mobly" onClick={() => window.open("https://www.mobly.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/etna.png" alt="Etna" onClick={() => window.open("https://www.etna.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/leroymerlin.png" alt="Leroy Merlin" onClick={() => window.open("https://www.leroymerlin.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/pontofrio.png" alt="Ponto Frio" onClick={() => window.open("https://www.pontofrio.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/magalu.png" alt="Magazine Luiza" onClick={() => window.open("https://www.magazineluiza.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/americanas.png" alt="Americanas" onClick={() => window.open("https://www.americanas.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/oppa.png" alt="Oppa" onClick={() => window.open("https://www.oppa.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/westwing.png" alt="Westwing" onClick={() => window.open("https://www.westwing.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/carrefour.png" alt="Carrefour" onClick={() => window.open("https://www.carrefour.com.br/", "_blank", "noopener,noreferrer")} />
-                            <Image width={200} height={200} className="cursor-pointer" src="/madesa.png" alt="Madesa" onClick={() => window.open("https://www.madesa.com/", "_blank", "noopener,noreferrer")} />
+                        <div className="flex gap-6 w-full mb-6">
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/madeiramadeira.png" alt="MadeiraMadeira" onClick={() => window.open("https://www.madeiramadeira.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/tokstok.png" alt="Tok&Stock" onClick={() => window.open("https://www.tokstok.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/casasbahia.png" alt="Casas Bahia" onClick={() => window.open("https://www.casasbahia.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/mobly.png" alt="Mobly" onClick={() => window.open("https://www.mobly.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/etna.png" alt="Etna" onClick={() => window.open("https://www.etna.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/leroymerlin.png" alt="Leroy Merlin" onClick={() => window.open("https://www.leroymerlin.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/pontofrio.png" alt="Ponto Frio" onClick={() => window.open("https://www.pontofrio.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/magalu.png" alt="Magazine Luiza" onClick={() => window.open("https://www.magazineluiza.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/americanas.png" alt="Americanas" onClick={() => window.open("https://www.americanas.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/oppa.png" alt="Oppa" onClick={() => window.open("https://www.oppa.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/westwing.png" alt="Westwing" onClick={() => window.open("https://www.westwing.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/carrefour.png" alt="Carrefour" onClick={() => window.open("https://www.carrefour.com.br/", "_blank", "noopener,noreferrer")} />
+                            <Image width={200} height={200} className="transition-transform duration-300 hover:scale-95 rounded-lg shadow-md cursor-pointer" src="/madesa.png" alt="Madesa" onClick={() => window.open("https://www.madesa.com/", "_blank", "noopener,noreferrer")} />
                         </div>
                     </div>
                    <IdeiasSlider></IdeiasSlider>
@@ -53,25 +53,25 @@ export default function Home() {
                 <div className="max-w-[20%]">
                     <h4 className="text-2xl text-gray-600 mb-8">Loja mais acessadas</h4>
                     <div id="rankingStories" className="w-100 rounded-2xl flex flex-col gap-4">
-                        <div id="madeiraMadeira" className="flex items-center bg-orange-200 w-full rounded-2xl">
+                        <div id="madeiraMadeira" className="flex items-center bg-orange-200 w-full rounded-2xl transition-transform duration-300 hover:translate-x-2">
                             <Image width={200} height={200} src="/madeiramadeira.png" className="w-28" alt="MadeiraMadeira"/>
                             <h5 className="ml-4 text-lg text-orange-600 font-medium">MadeiraMadeira</h5>
                         </div>
-                        <div id="tokstok" className="flex items-center bg-green-100 w-full rounded-2xl">
+                        <div id="tokstok" className="flex items-center bg-green-100 w-full rounded-2xl transition-transform duration-300 hover:translate-x-2">
                             <Image width={200} height={200} src="/tokstok.png" className="w-28" alt="Tok&Stok"/>
                             <h5 className="ml-4 text-lg text-emerald-600 font-medium">Tok&Stok</h5>
                         </div>
-                        <div id="tokstok" className="flex items-center bg-blue-100 w-full rounded-2xl">
+                        <div id="tokstok" className="flex items-center bg-blue-100 w-full rounded-2xl transition-transform duration-300 hover:translate-x-2">
                             <Image width={200} height={200} src="/casasbahia.png" className="w-28" alt="Casas Bahia"/>
                             <h5 className="ml-4 text-lg text-blue-600 font-medium">Casas Bahia</h5>
                         </div>
                     </div>
                     <h4 className="text-2xl text-gray-600 mt-8 mb-8">Suas Favoritas</h4>
                     <div id="yourFavorites" className="w-100 grid gap-4 grid-cols-3 rounded-2xl">
-                        <Image width={200} height={200} src="/mobly.png" alt="Mobly" />
-                        <Image width={200} height={200} src="/madesa.png" alt="Madesa" />
-                        <Image width={200} height={200} src="/leroymerlin.png" alt="Leroy Merlin" />
-                        <Image width={200} height={200} src="/magalu.png" alt="Magazine Luiza" />
+                        <Image width={200} height={200} src="/mobly.png" alt="Mobly" className='transition-transform duration-300 hover:-translate-y-2' />
+                        <Image width={200} height={200} src="/madesa.png" alt="Madesa" className='transition-transform duration-300 hover:-translate-y-2' />
+                        <Image width={200} height={200} src="/leroymerlin.png" alt="Leroy Merlin" className='transition-transform duration-300 hover:-translate-y-2' />
+                        <Image width={200} height={200} src="/magalu.png" alt="Magazine Luiza" className='transition-transform duration-300 hover:-translate-y-2' />
                     </div>
                 </div>
             </main>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -32,7 +32,7 @@ export default function Register() {
                 <Image src="/logo.png" width={200} height={200} alt="Logo da AcessoLar" />
                 <div className="mt-12 flex flex-col gap-10 pl-6">
                     <h1 className="font-bold text-4xl">Crie sua conta</h1>
-                    <button className="border rounded-2xl border-gray-600 p-5 w-2xl text-gray-600 flex gap-4 items-center justify-center text-2xl cursor-pointer ease-in-out duration-75 hover:border-gray-400">  <Image width={32} height={100} src="/logo-google.png" alt="Logo da Google" /> Continue com o Google</button>
+                    <button className="border rounded-2xl border-gray-600 p-5 w-2xl text-gray-600 flex gap-4 items-center justify-center text-2xl cursor-pointer ease-in-out duration-75 hover:border-gray-400 transition-transform duration-300 hover:translate-x-2">  <Image width={32} height={100} src="/logo-google.png" alt="Logo da Google" /> Continue com o Google</button>
                     <div className="flex items-center justify-center gap-2 w-2xl">
                         <hr className="bg-black w-16" />
                         <span className="text-2xl text-gray-600">Ou cadastre-se com seu email</span>
@@ -50,7 +50,7 @@ export default function Register() {
                     <span className="absolute bg-primary left-32 top-6/12 mt-50  text-2xl pl-2 pr-2">Senha</span>
                         <input className="border w-2xl p-5 rounded-2xl border-gray-800 text-2xl" type="password" title="Senha" onChange={(e) => setPassword(e.target.value)} />
                     </div>
-                    <button className="p-5 w-2xl bg text-white bg-teal-700 rounded-2xl text-2xl text-bold cursor-pointer ease-in-out duration-75 hover:bg-teal-800" type="button" onClick={register}>Registrar Agora</button>
+                    <button className="p-5 w-2xl bg text-white bg-teal-700 rounded-2xl text-2xl text-bold cursor-pointer ease-in-out duration-75 hover:bg-teal-800 transition-transform duration-300 hover:translate-x-2" type="button" onClick={register}>Registrar Agora</button>
                     <div className="flex items-center justify-center gap-2 w-2xl">
                         <hr className="bg-black w-16" />
                         <span className="text-2xl text-gray-600">Ou <a href="/login" className="underline">Entre na conta agora</a></span>
